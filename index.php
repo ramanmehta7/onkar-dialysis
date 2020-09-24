978a2e6dc4e85c81b9be8bb6cd5bf12c10e04dbd
ramanmehta7 <ramanmehta833@gmail.com>
2020-09-24 20:28:28 +0530

    resources

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php include("header.php"); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Shiv-Dialysis Center</title>
+</head>
+
+<body>
+
+</body>
+
+<?php include("footer.php"); ?>
+</html>
